5989190218b67728ea4243a78b811f87b3d6c19e
Ahmed Fahim <82629098+A7med31fimo@users.noreply.github.com>
2026-05-13 16:56:16 +0300

    continue with exception handling

--- src/Week1_Student_management_console_app/Day4/Public_definations.java
+++ src/Week1_Student_management_console_app/Day4/Public_definations.java
@@ -6,18 +6,42 @@
 *******************************************************************
    2- Exception Handling (try/catch/finally, custom exceptions)
     1- it is unexpected error occur in run time
-
-
-
-
-
-
-
-
-
+    2- database connection fail
+    3- null pointer
+    4- invalid input and so on.
+   every Exception inherit from throwable
+    Throwable
+     ├── Error
+     └── Exception
+           ├── RuntimeException
+           └── Checked Exceptions ->IOException,SQLException
  */
+static class InvalidAgeException extends Exception{
 
+    public InvalidAgeException(String msg) {
+        super(msg);
+    }
+}
+public class Valided{
+    static void checkAge(int age) throws InvalidAgeException {
+        if(age < 18){
+            throw new InvalidAgeException("Age must be +18");
+        }
+        IO.println("Allowed");
+    }
+}
+public static void main(String[] args) {
+//   try {
+//       int x = 0;
+//
+//       IO.println(20/x);
+//   } catch (Exception e) {
+//       IO.println(Arrays.toString(e.toString().split(":")));
+//   }
 
-void main() {
-
+    try {
+        Valided.checkAge(13);
+    }catch (InvalidAgeException e){
+        IO.println(e.getMessage());
+    }
 }
