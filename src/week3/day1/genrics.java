a5ac22bb7d2113a5b4f80dbffecf1bb1f81484f1
Ahmed Fahim <82629098+A7med31fimo@users.noreply.github.com>
2026-06-28 09:48:58 +0300

    push generics

--- src/week3/day1/genrics.java
+++ src/week3/day1/genrics.java
@@ -1,6 +1,20 @@
 package week3.day1;
+/*
+importance of generics
+1- type safety
+2- no casting
+3 compile time checking
 
+
+
+
+
+
+
+ */
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class Test<T>{
     private T[] t;
@@ -24,6 +38,13 @@ public class genrics {
     public static void main(String[] args) {
         Test<Integer> test=new Test<>();
         test.setT(new Integer[]{1,2,3,4,5});
-        Arrays.stream(test.getT()).forEach((e)->(System.out.print(e+" ")));
+//        Arrays.stream(test.getT()).forEach(System.out::println);
+        ArrayList list = new ArrayList();
+
+        list.add("Ahmed");
+        list.add(10);
+
+        String name = String.valueOf(list.get(1));
+        IO.println(name);
     }
 }
